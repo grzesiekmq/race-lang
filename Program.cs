ee4b1de1dba4caf29dada9c52993a2bd5ec3f46f
grzesiekmq <grzesiekfuta@gmail.com>
2025-08-26 23:17:44 +0200

    racec working pipeline (lacks still dynamic codegen)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
-using Generated;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
-
+using Generated;
 
 class Program
 {
-static void PrintTree(IParseTree tree, Parser parser, string indent = "", bool last = true)
+    static void PrintTree(IParseTree tree, Parser parser, string indent = "", bool last = true)
     {
         // Wypisz bieżący węzeł
         string nodeText = Trees.GetNodeText(tree, parser);
@@ -32,11 +32,12 @@ static void PrintTree(IParseTree tree, Parser parser, string indent = "", bool l
             PrintTree(tree.GetChild(i), parser, indent, i == tree.ChildCount - 1);
         }
     }
+
     static void Main(string[] args)
     {
         var sb = new StringBuilder();
-
-        var input = new AntlrFileStream("test.race");
+        var inputFile = args[0];
+        var input = new AntlrFileStream(inputFile);
 
         var lexer = new RaceLangLexer(input);
 
@@ -46,51 +47,50 @@ static void PrintTree(IParseTree tree, Parser parser, string indent = "", bool l
 
         var tree = parser.program();
 
-
         var visitor = new CodeGen();
 
         // Console.WriteLine(tree.ToStringTree());
 
         // PrintTree(tree, parser);
 
-/*
-tokens.Fill();
-foreach (var t in tokens.GetTokens())
-{
+        /*
+        tokens.Fill();
+        foreach (var t in tokens.GetTokens())
+        {
+        
+            Console.WriteLine($"{t.Text} : {t.Type}");
+        }
+        */
 
-    Console.WriteLine($"{t.Text} : {t.Type}");
-}
-*/
- 
-            // var structDecl = (StructDeclNode) visitor.Visit(tree);
+        // var structDecl = (StructDeclNode) visitor.Visit(tree);
 
-            // var addSub = (BinaryExprNode) visitor.Visit(expression);
-            //  var left = (NumberLiteralExpr) addSub.Left;
-            //  var op = addSub.Op;
-            //  var right = (NumberLiteralExpr) addSub.Right;
-
+        // var addSub = (BinaryExprNode) visitor.Visit(expression);
+        //  var left = (NumberLiteralExpr) addSub.Left;
+        //  var op = addSub.Op;
+        //  var right = (NumberLiteralExpr) addSub.Right;
 
         //    int MapOp(string op) => op switch{
         //         "+" => int.Parse(left.Value) + int.Parse(right.Value)
         //     };
 
+        //  Console.WriteLine(  MapOp(op)  );
 
-            //  Console.WriteLine(  MapOp(op)  );
-
-        string MapType(string raceType) => raceType switch{
-            "i32" => "int",
-            "f32" => "float",
-
-        };
-        // string fieldsCode = string.Join("\n", structDecl.Fields.Select(f => 
-        //             $"{MapType(f.Type)} {f.Name};" 
+        string MapType(string raceType) =>
+            raceType switch
+            {
+                "i32" => "int",
+                "f32" => "float",
+            };
+        // string fieldsCode = string.Join("\n", structDecl.Fields.Select(f =>
+        //             $"{MapType(f.Type)} {f.Name};"
         // ));
 
         // Console.WriteLine(fieldsCode);
 
-// hardcoded temporarily for reference
-// to convert to dynamic code
-string code = @"
+        // hardcoded temporarily for reference
+        // to convert to dynamic code
+        string code =
+            @"
 #include <stdio.h>
 
 // ----------- struct declaration ------------
@@ -133,13 +133,60 @@ int main() {
     return 0;
 }
 ";
-        Console.WriteLine(code);
-        
-        File.WriteAllText("test.c", code);
-  
-  
+        // Console.WriteLine(code);
+
+        var cFile = "test.c";
+        var outputFile = args[2];
+        File.WriteAllText(cFile, code);
+
+        // Sprawdzenie, czy plik C istnieje
+        if (!File.Exists(cFile))
+        {
+            Console.WriteLine($"File {cFile} not found!");
+            return;
         }
-    
-    
-    
+
+
+
+
+        // Wywołanie GCC
+        var gcc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "gcc",
+                Arguments = $"{cFile} -o {outputFile}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            },
+        };
+
+        gcc.Start();
+
+        // Asynchronicznie odczytujemy stdout/stderr
+        string stdout = gcc.StandardOutput.ReadToEnd();
+        string stderr = gcc.StandardError.ReadToEnd();
+
+        // Czekamy aż GCC zakończy
+        gcc.WaitForExit();
+
+        if (gcc.ExitCode != 0)
+        {
+            Console.WriteLine("Compilation failed:");
+            Console.WriteLine(stderr);
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(stdout))
+            Console.WriteLine(stdout);
+
+        if (!string.IsNullOrEmpty(stderr))
+            Console.WriteLine(stderr);
+
+        Console.WriteLine($"Binary created: {outputFile}");
+
+
+    }
 }
